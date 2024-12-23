d27ed9f9fb6c2992b99b241a4dc7f90ebc3ef69a
TomasCImach <tomas90@gmail.com>
2024-12-23 11:55:59 -0300

    create router and Landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,18 @@
 import React from 'react'
+import { Navigate, Route, Routes } from "react-router-dom";
 import './App.css'
-import Hero from './sections/Hero'
-import OverlayLogoSection from './sections/OverlayLogoSection'
 import Header from './sections/Header'
-import TrendingSection from './sections/TrendingSection'
-import HowItWorksSection from './sections/HowItWorksSection'
-import GovernanceSection from './sections/GovernanceSection'
-import TradeEverythingSection from './sections/TradeEverythingSection'
-import AuditsSection from './sections/AuditSection'
-import WhitePaperSection from './sections/WhitePaperSection'
 import Footer from './sections/Footer'
-import DynamicIndexesSection from './sections/DynamicIndexesSection'
-import VcSection from './sections/VcSection'
+import Landing from './pages/Landing';
 
 const App: React.FC = () => {
   return (
     <div style={{ width: '100%', overflowX: 'hidden', position: 'relative' }}>
       <Header />
-      <Hero />
-      <OverlayLogoSection />
-      <VcSection />
-      <DynamicIndexesSection />
-      <TrendingSection />
-      <HowItWorksSection />
-      <GovernanceSection />
-      <TradeEverythingSection />
-      <AuditsSection />
-      <WhitePaperSection />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/*" element={<Navigate to="/" />} />
+      </Routes>
       <Footer />
     </div>
   )
